b6dc04f7367de620f1629d2fa1b43818168d2d16
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2021-12-17 11:21:03 +0100

    ** Storing Data in Files Via the Model

--- models/product.js
+++ models/product.js
@@ -1,15 +1,36 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+const { createBrotliCompress } = require("zlib");
+const rootDir = require("../util/path");
+
+const p = path.join(rootDir, "data", "products.json");
+
+const getProductsFromFile = (callback) => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      console.log(err);
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
-    this.title = t;
+    this.title = title;
   }
 
   save() {
-    products.push(this);
+    getProductsFromFile((products) => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        if (err) console.log("Error::: " + err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(callback) {
+    getProductsFromFile(callback);
   }
 };
