708071067ba5c3a48b6b366469854ee3f45107ad
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2021-12-17 04:12:06 +0100

    ** Try out handlebars templating engine

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const path = require("path");
 const express = require("express");
 const app = express();
+const expressHbs = require("express-handlebars");
 
-app.set("view engine", "pug");
-app.set("views", "views");
+// using pug templating engine
+// app.set("view engine", "pug");
+
+app.engine(
+  "hbs",
+  expressHbs.engine({
+    layoutsDir: "views/layouts/",
+    defaultLayout: "main-layout",
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
 
 const adminData = require("./routes/admin.js");
 const shopRouter = require("./routes/shop");
@@ -16,7 +27,7 @@ app.use("/admin", adminData.routes);
 app.use("/", shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).render("error-404.pug", {pageTitle: 'Page Not Found'});
+  res.status(404).render("error-404.pug", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,14 @@ const adminData = require("./admin");
 router.get("/", (req, res, next) => {
   const products = adminData.products;
   console.log(products);
-  res.render("shop.pug", { prods: products, pageTitle: "My Shop", path: "/" });
+  res.render("shop", {
+    prods: products,
+    pageTitle: "My Shop",
+    path: "/",
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
