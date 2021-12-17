9737c864d837e8cbd8439fc0e7d598146294b5c1
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2021-12-17 06:34:30 +0100

    ** Clean Up Templating (use Ejs)

--- app.js
+++ app.js
@@ -2,24 +2,6 @@ const path = require("path");
 const express = require("express");
 const app = express();
 
-// using pug templating engine
-// app.set("view engine", "pug");
-
-// Using handlebars templating engine
-// const expressHbs = require("express-handlebars");
-// app.engine(
-//   "hbs",
-//   expressHbs.engine({
-//     layoutsDir: "views/layouts/",
-//     defaultLayout: "main-layout",
-//     extname: "hbs",
-//   })
-// );
-// app.set("view engine", "hbs");
-
-// using ejs templating engine
-app.set("view engine", "ejs");
-
 const adminData = require("./routes/admin.js");
 const shopRouter = require("./routes/shop");
 
@@ -31,7 +13,7 @@ app.use("/admin", adminData.routes);
 app.use("/", shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).render("error-404", { pageTitle: "Page Not Found" });
+  res.status(404).render("error-404.pug", { pageTitle: "Page Not Found" });
 });
 
 app.listen(3000);
