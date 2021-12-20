35e29e692d69e0fe97305a9c6fcc2a23307b3c5a
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2021-12-20 09:21:26 +0100

    ** Enhancing The project

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const productController = require('../controllers/products.js');
+const adminController = require("../controllers/admin.js");
 
 /// route:: /admin/add-product => GET
-router.get("/add-product", productController.getAddProduct);
+router.get("/add-product", adminController.getAddProduct);
+
+/// route:: /admin/products => GET
+router.get("/products", adminController.getProducts);
 
 /// route:: /admin/add-product => POST
-router.post("/add-product", productController.postAddProduct);
+router.post("/add-product", adminController.postAddProduct);
 
 module.exports = router;
-
--- routes/shop.js
+++ routes/shop.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const productController = require("../controllers/products.js");
+const shopController = require("../controllers/shop.js");
 
-router.get("/", productController.getProducts);
+/// route:: / => GET
+router.get("/", shopController.getIndex);
+
+/// route:: /products => GET
+router.get("/products", shopController.getProducts);
+
+/// route:: /checkout => GET
+router.get("/checkout", shopController.getCheckout);
+
+/// route:: /cart => GET
+router.get("/cart", shopController.getOrders);
+
+/// route:: /orders => GET
+router.get("/orders", shopController.getCart);
 
 module.exports = router;
