035dae20ef3497cd31aebf1a573f2879165eb770
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2021-12-21 09:31:53 +0100

    ** Delete Product Feature

--- models/cart.js
+++ models/cart.js
@@ -34,4 +34,25 @@ module.exports = class Cart {
       });
     });
   }
+
+  static deleteProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      if (err) return;
+
+      const updatedCart = { ...JSON.parse(fileContent) };
+      const product = updatedCart.product.find((prod) => prod.id === id);
+      const productQty = product.qty;
+      // delete product from cart by filtering it out
+      updatedCart.products = updatedCart.products.filter(
+        (prod) => prod.id !== id
+      );
+      // reduce Cart's total price
+      updatedCart.totalPrice =
+        updatedCart.totalPrice - productPrice * productQty;
+
+      fs.writeFile(p, JSON.stringify(updatedCart), (err) => {
+        if (err) console.log(err);
+      });
+    });
+  }
 };
--- routes/admin.js
+++ routes/admin.js
@@ -17,4 +17,8 @@ router.get("/edit-product/:productId", adminController.getEditProduct);
 /// route:: /admin/edit-product => POST
 router.post("/edit-product", adminController.postEditProduct);
 
+
+/// route:: /admin/delete-product => POST
+router.post("/delete-product", adminController.postDeleteProduct);
+
 module.exports = router;
