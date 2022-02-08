05230e605e0157c8fc2b7b603b0fa198005b4d14
Lone Wolf <53178226+Lone-Wolf17@users.noreply.github.com>
2022-02-08 15:05:10 +0100

    ** Add Validation to SignUp Page

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require("bcryptjs");
 const crypto = require("crypto");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 const Routes = require("../constants/routes");
@@ -66,6 +67,16 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors.array()[0]);
+    return res.status(422).render("auth/signup", {
+      path: Routes.signup,
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
 
   User.findOne({ email: email })
     .then((userDoc) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const authController = require('../controllers/auth');
-const Routes = require('../constants/routes');
+const express = require("express");
+const { check, body } = require("express-validator");
+const authController = require("../controllers/auth");
+const Routes = require("../constants/routes");
 
 const router = express.Router();
 
@@ -14,7 +15,25 @@ router.get(Routes.signup, authController.getSignup);
 router.post(Routes.login, authController.postLogin);
 
 /// route:: /signup => POST
-router.post(Routes.signup, authController.postSignup);
+router.post(
+  Routes.signup,
+  [
+    check("email").isEmail().withMessage("Please enter a valid Email"),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and atleast 5 characters"
+    )
+      .isLength({ min: 8 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Passwords have to match");
+      }
+      return true;
+    }),
+  ],
+  authController.postSignup
+);
 
 /// route:: /logout => POST
 router.post(Routes.logout, authController.postLogout);
@@ -26,7 +45,7 @@ router.get(Routes.resetPassword, authController.getResetPassword);
 router.post(Routes.resetPassword, authController.postReset);
 
 /// route:: /new-password/:token => GET
-router.get(Routes.newPassword + '/:token', authController.getNewPassword);
+router.get(Routes.newPassword + "/:token", authController.getNewPassword);
 
 /// route:: /new-password => POST
 router.post(Routes.newPassword, authController.postNewPassword);
